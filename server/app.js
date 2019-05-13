842018496ee00dea0dcf988f591e236f247976b3
John Park <jhnprk12@gmail.com>
2019-05-13 13:33:29 -0700

    work in progress

--- server/app.js
+++ server/app.js
@@ -2,18 +2,25 @@
 
 
 // create your express server below
-let express = require("express");
-let app = express();
-
+var app;
 
 // add your routes and middleware below
-    app.get('/', function(req, res) {
-        res.send('Hello World!');
-      });
-      
-      app.listen(3000, function() {
-        console.log('Example app listening on port 3000!');
-      });
 
 // finally export the express application
 module.exports = app;
+
+
+// In order to respond to requests with the top spots data, you'll need to import the data.json file using require().
+
+// Exit Criteria
+// Server should respond to GET requests to / route the top spots page
+
+// Server should respond with a status code of 200
+
+// Server should log each request using morgan's dev format
+
+// Server should indicate when it is listening and on which port
+
+// Server should respond to GET requests to /data route with the top spots data
+
+// All tests should pass
