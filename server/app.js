28ed2583ee0bcaa319136099e0fc6ac37d296b94
John Park <jhnprk12@gmail.com>
2019-05-13 11:23:07 -0700

    work in progress

--- server/app.js
+++ server/app.js
@@ -2,9 +2,18 @@
 
 
 // create your express server below
-var app;
+let express = require("express");
+let app = express();
+
 
 // add your routes and middleware below
+    app.get('/', function(req, res) {
+        res.send('Hello World!');
+      });
+      
+      app.listen(3000, function() {
+        console.log('Example app listening on port 3000!');
+      });
 
 // finally export the express application
 module.exports = app;
