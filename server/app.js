15e8ed16270775e1c23d679b359b15631b07c1c2
John Park <jhnprk12@gmail.com>
2019-05-14 10:17:23 -0700

    making progress

--- server/app.js
+++ server/app.js
@@ -1,13 +1,44 @@
 // import files and packages up here
-
+const express = require("express");
+const morgan = require("morgan");
+const data = require("./data.json");
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
 
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/data", (req, res) => {
+    res.send(data);
+});
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
 // finally export the express application
 module.exports = app;
+
+
+
+
+
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+//   });
+  
+//   app.get("/data", (req, res) => {
+//     res.json(data);
+//   });
+
+// 1) GET / responds with a 200 response code
+//     2) GET /data responds with all top spots
 
 
 // In order to respond to requests with the top spots data, you'll need to import the data.json file using require().
