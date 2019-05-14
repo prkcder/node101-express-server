518db49b3627ce548ca16c06a36b67c5ed100237
John Park <jhnprk12@gmail.com>
2019-05-14 12:03:40 -0700

    review again

--- server/app.js
+++ server/app.js
@@ -18,17 +18,3 @@ app.get("/data", (req, res) => {
 
 // finally export the express application
 module.exports = app;
-
-
-// In order to respond to requests with the top spots data, you'll need to import the data.json file using require().
-
-// Exit Criteria
-// Server should respond to GET requests to / route the top spots page
-
-// Server should respond with a status code of 200
-
-// Server should log each request using morgan's dev format
-
-// Server should indicate when it is listening and on which port
-
-// Server should respond to GET requests to /data route with the top spots data
